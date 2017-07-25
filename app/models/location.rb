d34d2eaddd0e19147b993d1cb0e32099beeaad6c
ZacharyWeiner <zach@cloud.com>
2017-07-25 12:19:48 -0300

    added seed data for locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,4 @@
 class Location < ApplicationRecord
     has_many :children, :class_name => "Location", :foreign_key => "parent_id"
-    belongs_to :parent, :class_name => 'Location', :foreign_key => "parent_id"
+    belongs_to :parent, :class_name => 'Location', :foreign_key => "parent_id", optional: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+asia = Location.create!(name:"Asia", location_type: 'region')
+thailand = Location.create!(name:"Thailand", location_type: 'country', parent: asia)
+bangkok = Location.create!(name:"Bangkok", location_type: 'city', parent: thailand)
+changmai = Location.create!(name:"Chang Mai", location_type: 'city', parent: thailand)
+kohphanagan = Location.create!(name:"Koh Phangan", location_type: 'city', parent: thailand)
+
+europe = Location.create!(name:"Europe", location_type: 'region')
+england = Location.create!(name:"England", location_type: 'country', parent: europe)
+london = Location.create!(name:"London", location_type: 'city', parent: england)
+france = Location.create!(name:"France", location_type: 'country', parent: europe)
+paris = Location.create!(name:"Paris", location_type: 'city', parent: france)
