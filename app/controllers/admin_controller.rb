14ae0dae47e53abd7fecd2615849c4eb675b21da
ZacharyWeiner <zach@cloud.com>
2017-07-26 10:56:03 -0300

    added method name to controller for add_manager and added a page for administrating managers

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,9 @@
 class AdminController < ApplicationController
   def cohorts 
   end 
+
+  def add_manager 
+    @cohort = Cohort.find(params[:cohort_id])
+    @user = User.find(params[:user_id])
+  end 
 end
