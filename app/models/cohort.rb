1140639188a66e5969004ebe6a912c57ddf25c68
ZacharyWeiner <zach@cloud.com>
2017-07-26 10:06:48 -0300

    updated cohort show page layout, added a mount_uploader for banner_image, added banner image and profile image to show page

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,5 +1,6 @@
 class Cohort < ApplicationRecord
   mount_uploader :image, PictureUploader
+  mount_uploader :banner_image, PictureUploader
   has_and_belongs_to_many :users
   has_many :events
 end
