c2031a2a8dc2f4c1dfeee23414892c88805af8c9
ZacharyWeiner <zach@cloud.com>
2017-07-26 11:26:08 -0300

    added route for cohort manager pages and

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,5 +5,6 @@ class AdminController < ApplicationController
   def add_manager 
     @cohort = Cohort.find(params[:cohort_id])
     @user = User.find(params[:user_id])
+    @role = Role.find(title: 'cohort_admin')
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'users/:id', to: 'users#show', as: 'show_user'
   get 'admin/cohorts'
+  get 'admin/cohort_managers'
   root to:'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
