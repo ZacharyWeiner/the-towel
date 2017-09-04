71700ed9f65a3fa346949db127b9e9286391d3cf
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-09-04 15:47:30 -0300

    throws alert on connected to test execution

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,4 +1,7 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
+  connected: function (data){
+    alert('connected')
+  },
   received: function(data) {
   location.reload()
   },
