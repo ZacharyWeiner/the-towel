7d9f304f62b4c1d653885ee45555ebb5f838b30b
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-09-04 16:34:59 -0300

    moved subscribe js code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,15 +27,11 @@ if (typeof jQuery === "undefined") {
 
 $(document).ready(function() {
   App.messages = App.cable.subscriptions.create('MessagesChannel', {
-  connected: function (data){
-    alert('connected')
-  },
-  received: function(data) {
-  location.reload()
-  },
-
-  renderMessage: function(data) {
-    return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
+    connected: function (data){
+      alert('connected')
+    },
+    received: function(data) {
+    location.reload()
+    },
   }
-});
 });
