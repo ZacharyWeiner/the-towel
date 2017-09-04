a2fb7fa4ac7bb681a1e7eab15afa09b78fd8b5f6
Zachary Weiner <el_hefe@Zacharys-MacBook-Pro-2.local>
2017-09-04 16:47:59 -0300

    new jquery alert and hid channels js call

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,12 +1,12 @@
-App.messages = App.cable.subscriptions.create('MessagesChannel', {
-  connected: function (data){
-    alert('connected')
-  },
-  received: function(data) {
-  location.reload()
-  },
+// App.messages = App.cable.subscriptions.create('MessagesChannel', {
+//   connected: function (data){
+//     alert('connected')
+//   },
+//   received: function(data) {
+//   location.reload()
+//   },
 
-  renderMessage: function(data) {
-    return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
-  }
-});
+//   renderMessage: function(data) {
+//     return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
+//   }
+// });
